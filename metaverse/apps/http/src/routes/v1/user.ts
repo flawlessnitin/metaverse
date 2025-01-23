763fae5b2a06eb3c11a03d06317ff0173bc40cc3
Nitin Sahu <sahunitin970@gmail.com>
2025-01-23 13:44:39 +0530

    feat(user): update metadata route to handle avatarId as an object

--- metaverse/apps/http/src/routes/v1/user.ts
+++ metaverse/apps/http/src/routes/v1/user.ts
@@ -4,7 +4,7 @@ import client from "@repo/db/client";
 import { updateMetadataSchema } from "../../types";
 export const userRouter = Router();
 
-userRouter.post("/metadata", userMiddleware, (req, res) => {
+userRouter.post("/metadata", userMiddleware, async (req, res) => {
   const parsedData = updateMetadataSchema.safeParse(req.body);
   if (!parsedData.success) {
     res.status(400).json({ message: "Validation failed!" });
@@ -14,7 +14,7 @@ userRouter.post("/metadata", userMiddleware, (req, res) => {
     where: {
       id: req.userId,
     },
-    data: parsedData.data.avatarId,
+    data: { avatarId: parsedData.data.avatarId },
   });
 });
 userRouter.get("/metadata/bulk", (req, res) => {});
